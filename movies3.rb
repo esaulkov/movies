56c68884de1b9e891389372c96bc739260321743
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2017-06-03 21:22:01 +0700

    fix: simplify code

--- movies3.rb
+++ movies3.rb
@@ -4,6 +4,7 @@
 require 'date'
 
 DEFAULT_PATH = 'movies.txt'.freeze
+MOVIE_PARAMS = %i[link name year country release genres length rating producer actors].freeze
 
 def movies_list(films)
   films.map do |movie|
@@ -15,9 +16,8 @@ search_name = ARGV[0]
 movies_file = ARGV[1] || DEFAULT_PATH
 abort("This file doesn't exist") unless File.file?(movies_file)
 
-keys = %i[link name year country release genres length rating producer actors]
 movies = File.open(movies_file, 'r').map do |line|
-  keys.zip(line.split('|')).to_h
+  MOVIE_PARAMS.zip(line.split('|')).to_h
 end
 
 sorted = movies.sort_by { |movie| movie[:length].to_i }.last(5).reverse
@@ -25,21 +25,17 @@ sorted = movies.sort_by { |movie| movie[:length].to_i }.last(5).reverse
 puts 'The five longest movies are:'
 puts movies_list(sorted)
 
-comedies = movies.select { |movie| movie[:genres].include?('Comedy') }
-
-sorted = comedies.sort_by do |movie|
-  if movie[:release].include?('-')
-    Date.parse(movie[:release])
-  else
-    DateTime.new(movie[:release].to_i)
-  end
-end
+comedies = movies
+  .select { |movie| movie[:genres].include?('Comedy') }
+  .sort_by { |movie| movie[:release] }
+  .first(10)
 
 puts "\nThe first ten comedies are:"
-puts movies_list(sorted.first(10))
+puts movies_list(comedies)
 
 producers = movies
-  .collect { |movie| movie[:producer] }.uniq
+  .collect { |movie| movie[:producer] }
+  .uniq
   .sort_by { |producer| producer.split.last }
 
 puts "\nList of producers:"
